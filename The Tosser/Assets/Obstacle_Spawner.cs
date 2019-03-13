7dcc05e0058b04f11fd84fe53523b435a802b8fe
Tyler S <tyler_r_smart@outlook.com>
2019-03-14 09:21:50 +1030

    Literally Everything

---
+++ The Tosser/Assets/Obstacle_Spawner.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Spawner : MonoBehaviour {
+
+    public GameObject dangerCube;
+    public GameObject self;
+    public bool spawnerOn;
+    public float spawnChance;
+    float willItSpawn;
+    public float difficultyIncrementer;
+    public bool difficultyOn;
+    public float backupDifficulty;
+    //The array for obstacles to spawn
+    public GameObject[] obstacles;
+    public bool BeatMode = false;
+    public bool inBeat;
+    public int counter;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        backupDifficulty = difficultyIncrementer;
+        self = gameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (spawnerOn == true)
+        {
+            if (BeatMode == false)
+            {
+                willItSpawn = Random.Range(1.0f, spawnChance + difficultyIncrementer);
+                if (difficultyOn)
+                {
+                    difficultyIncrementer += (difficultyIncrementer / 100) * Time.deltaTime;
+                }
+                if (willItSpawn >= spawnChance)
+                {
+                    int whichOne = Random.Range(1, 10);
+                    if (whichOne <= 8)
+                    {
+                        Instantiate(obstacles[0], self.transform);
+                    }
+                    if (whichOne > 8)
+                    {
+                        Instantiate(obstacles[1], self.transform);
+                    }
+                }
+            }
+            //if (BeatMode == true)
+            //{
+            //    counter++;
+            //    if (counter > 25)
+            //    {
+            //        inBeat = true;
+            //        if (counter > 30)
+            //        {
+            //            counter = 0;
+            //        }
+                    
+            //    }
+            //    if (counter < 30)
+            //    {
+            //        inBeat = false;
+            //    }
+            //    willItSpawn = Random.Range(1.0f, spawnChance + difficultyIncrementer);
+            //    if (difficultyOn)
+            //    {
+            //        difficultyIncrementer += (difficultyIncrementer / 1000);
+            //    }
+            //    if (willItSpawn >= spawnChance)
+            //    {
+            //        if (inBeat == true)
+            //        {
+                        
+            //            int whichOne = Random.Range(1, 10);
+            //            if (whichOne <= 8)
+            //            {
+            //                Instantiate(obstacles[0], self.transform);
+            //            }
+            //            if (whichOne > 8)
+            //            {
+            //                Instantiate(obstacles[1], self.transform);
+            //            }
+                        
+            //        }
+            //    }
+            //}
+        }
+
+        
+	}
+    
+}
---
+++ The Tosser/Assets/Pipe_Rotation.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pipe_Rotation : MonoBehaviour {
+
+    GameObject self;
+    public float rotateForce;
+    public bool isAndroid;
+    public bool accelOptions;
+    public Button leftButton;
+    public Button rightButton; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        self = gameObject;
+        
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //Using the left and right arrows to turn the whole tunnel
+        if (isAndroid == false)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                self.transform.Rotate(0, -rotateForce, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                self.transform.Rotate(0, rotateForce, 0);
+            }
+            
+        }
+        if (isAndroid == true)
+        {
+            if (accelOptions == true)
+            {
+                if (Input.acceleration.x > 0)
+                {
+                    self.transform.Rotate(0, -rotateForce, 0);
+                }
+                if (Input.acceleration.x < 0)
+                {
+                    self.transform.Rotate(0, -rotateForce, 0);
+                }
+            }
+            if (accelOptions == false)
+            {
+                self.transform.Rotate(0, Input.acceleration.x * rotateForce, 0);
+
+            }
+        }
+
+
+    }
+
+    public void LeftButton ()
+    {
+        self.transform.Rotate(0, -22.5f, 0);
+    }
+
+    public void RightButton ()
+    {
+        self.transform.Rotate(0, 22.5f, 0);
+    }
+
+}
