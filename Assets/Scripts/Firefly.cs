27239a05d997005f430943ebca6c8936f8efb177
antoine odi <antoine.kenneth.odi@gmail.com>
2019-03-27 13:55:08 +1030

    Firefly movement

--- Assets/Scripts/Firefly.cs
+++ Assets/Scripts/Firefly.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Firefly : MonoBehaviour {
 
-    public GameObject[] positioningObjects;
-    GameObject self;
-    float willItMove;
-    public float moveChance;
+    [SerializeField] private Transform positioningObjectsParent = null;
+    [SerializeField] private List<Transform> m_positioningObjects = new List<Transform>();
+    [SerializeField] private int moveChance = 50;
+    int willItMove;
     int currentChance;
     Vector3 newPosition;
     public float moveSpeed;
@@ -16,27 +16,26 @@ public class Firefly : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        self = gameObject;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+        if (positioningObjectsParent != null)
+        {
+            foreach (Transform child in positioningObjectsParent)
+            {
+                m_positioningObjects.Add(child);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
     {
-        
-        willItMove = Random.Range(1.0f, moveChance + 1.0f);
+
+        willItMove = Random.Range(1, moveChance + 1);
         if (willItMove >= moveChance)
         {
-            currentChance = Random.Range(0, positioningObjects.Length);
-            Vector3 assignedPosition = positioningObjects[currentChance].transform.position;
-            newPosition = new Vector3(assignedPosition.x, assignedPosition.y, assignedPosition.z);
+            currentChance = Random.Range(0, m_positioningObjects.Count);
+            newPosition = m_positioningObjects[currentChance].transform.position;
                 
         }
-        self.transform.position = Vector3.Lerp(self.transform.position, newPosition, moveSpeed);
-        //if (self.transform.position != newPosition)
-        //{
-        //    self.transform.position = Vector3.Lerp(self.transform.position, newPosition, 0.5f);
-        //    //Debug.Log("It should be moving...");
-        //}
-
+        transform.localPosition = Vector3.Lerp(transform.localPosition, newPosition, moveSpeed);
     }
 }
