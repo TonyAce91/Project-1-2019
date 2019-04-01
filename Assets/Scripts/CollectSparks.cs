8b8894bd6c49d1858edd29df34b5b4b59dec8d64
Andrew <lazzyboy1630@gmail.com>
2019-04-01 19:32:30 +1030

    tried to add collection to sparks not quite working

---
+++ Assets/Scripts/CollectSparks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class CollectSparks : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        ScoreSystem.theScore += 50;
+        Destroy(gameObject);
+    }
+}
+
+    
---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ScoreSystem : MonoBehaviour {
+
+    public GameObject ScoreText;
+    public static int theScore;
+  
+
+    void Update()
+    {
+        ScoreText.GetComponent<Text>().text = "Score: " + theScore;
+    }
+
+}
+
